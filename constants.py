3691d96bbad7aa282274ecf438b148e30f852904
XuanWang <3200102316@zju.edu.cn>
2022-11-01 22:46:54 +0800

    :alembic: Change resolution ratio

--- constants.py
+++ constants.py
@@ -1,13 +1,13 @@
 # Camera parameters
 CAMERA_ID = 0
-CAMERA_W = 1920//2
-CAMERA_H = 1080//2
+CAMERA_W = 2592//8
+CAMERA_H = 1944//8
 
 # Projector parameters
 SCREEN_ID = 1
 WINDOW_NAME = 'projector'
-PROJECTOR_W = 1280
-PROJECTOR_H = 720
+PROJECTOR_W = 1920
+PROJECTOR_H = 1080
 
 # Capture algorithm parameters
 ZERO_THRESHOLD = 0.01 # Below this, consider the pixel as 0
